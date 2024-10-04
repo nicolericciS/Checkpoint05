1c1c31bc7931225f469b4f7f641fd074ba55d30c
nicolericciS <nicolericcisouza@gmail.com>
2024-10-03 22:13:43 -0300

    Primeira parte do projeto

---
+++ app/src/main/java/br/com/nicole/checkpoint5/MainActivity.kt
@@ -0,0 +1,24 @@
+package br.com.nicole.checkpoint5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+
+    }
+
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
+    }
+}
+
